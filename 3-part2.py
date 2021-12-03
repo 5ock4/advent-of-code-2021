de0471b83f811d6fdafa14688ac89e5b0f8b6641
Adam Strakoš <astrakos@linuxbox.cz>
2021-12-03 17:13:25 +0100

    3

--- 3-part2.py
+++ 3-part2.py
@@ -1,31 +1,55 @@
-    
+	
 with open('./input.txt') as f:
 	lines = [line.strip() for line in f]
-    
 
-def calc_arr(lines):
-    arr = ['']*len(lines[0])
+def most_common(arr, pos):
+	pos_str = ''
+	for num in arr:
+		pos_str += num[pos]
 
-    for bin in lines:
-        for i, char in enumerate(bin):
-            arr[i] += char
+	if pos_str.count('1') > pos_str.count('0'):
+		return '1'
+	elif pos_str.count('1') < pos_str.count('0'):
+		return '0'
+	else:
+		return None
 
-    return arr
+def least_common(arr, pos):
+	pos_str = ''
+	for num in arr:
+		pos_str += num[pos]
 
-arr = calc_arr(lines)
+	if pos_str.count('1') < pos_str.count('0'):
+		return '1'
+	elif pos_str.count('1') > pos_str.count('0'):
+		return '0'
+	else:
+		return None
 
-def calc(arr, lines):
-    l = []
-    for i, pos in enumerate(arr):
-        if pos.count('1') >= pos.count('0'):
-            for bin in lines:
-                if bin[i] == '1':
-                    l.append(bin)
-    
-    if len(lines) == 1:
-        return lines
+oxygen = ''
+co2 = ''
+row_len = len(lines[0])
+most = ''
 
-    calc_arr(lines)
-    return calc(arr, lines)
+for i in range(row_len):
+	most = most_common(lines, i) or '1'
+	lines = [ line for line in lines if line[i] == most ]
 
-print(calc(arr, lines))
+	if len(lines) == 1:
+		oxygen = int(lines[0], 2)
+
+print(oxygen)
+
+with open('./input.txt') as f:
+	lines = [line.strip() for line in f]
+
+for i in range(row_len):
+	least = least_common(lines, i) or '0'
+	lines = [ line for line in lines if line[i] == least ]
+
+	if len(lines) == 1:
+		co2 = int(lines[0], 2)
+
+print(co2)
+
+print(oxygen*co2)
